c6025f0e0e76c26888a512f957474227c9fcee81
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-03-26 13:59:31 +0700

    Tugas 2 Javascript
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+Contohnumber = 2021
+Contohstring = 'Maret'
+Contohboolean = true
+Contohnull = null;
+var unndefined;
+Contoharray = [16,4,20,21,4,22]
+
+console.log(Contohnumber)
+console.log(Contohstring)
+console.log(Contohboolean)
+console.log(Contohnull)
+console.log(unndefined)
+console.log(Contoharray)
